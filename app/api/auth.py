8800f205bbd1aee5b822c79726a57983ba3bb500
Ori Hadad <orih90@gmail.com>
2024-07-09 17:38:58 +0300

    change way to login

--- app/api/auth.py
+++ app/api/auth.py
@@ -6,18 +6,20 @@ from jose import JWTError, jwt
 from app.utils.security import verify_password, create_access_token, decode_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 from app.crud.user import get_user_by_email
 from app.schemas.user import User
-from app.schemas.token import Token
+from app.schemas.token import Token, LoginRequest
 from app.db.database import get_db
-from typing import Optional
+from typing import Optional, Any
+
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @router.post("/token", response_model=Token)
-def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = get_user_by_email(db, form_data.username)
-    if not user or not verify_password(form_data.password, user.hashed_password):
+def login_for_access_token2(login: LoginRequest, db: Session = Depends(get_db)):
+    print(f"Received login request: {login}")
+    user = get_user_by_email(db, login.username)
+    if not user or not verify_password(login.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
@@ -26,6 +28,7 @@ def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2Passw
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     payload = decode_access_token(token)
